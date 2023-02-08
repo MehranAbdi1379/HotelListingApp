99859c8ed532d43aafa727dd5f39a13bc62541bd
Mehran Abdi <mehranabdi13793@gmail.com>
2023-02-08 20:24:27 +0330

    Completing Country And Hotel DataBases

--- HotelListingApp/Data/Hotel.cs
+++ HotelListingApp/Data/Hotel.cs
@@ -1,5 +1,17 @@
-﻿namespace HotelListingApp.Data;
+﻿using Microsoft.EntityFrameworkCore.Storage.ValueConversion.Internal;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HotelListingApp.Data;
 
 public class Hotel
 {
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Address { get; set; }
+    public double Rating { get; set; }
+
+
+    [ForeignKey(nameof(Country))]
+    public int CountryId { get; set; }
+    public Country Country { get; set; }
 }
