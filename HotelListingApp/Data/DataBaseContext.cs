ea1ef99c5cbb93dd47eaa2005e9747e3db0a5941
Mehran Abdi <mehranabdi13793@gmail.com>
2023-02-08 20:28:23 +0330

    Create DataBase and Migrate

--- HotelListingApp/Data/DataBaseContext.cs
+++ HotelListingApp/Data/DataBaseContext.cs
@@ -7,11 +7,6 @@ public class DataBaseContext : DbContext
 	public DataBaseContext(DbContextOptions options) : base(options)
 	{}
 
-	protected override void OnConfiguring()
-	{
-
-	}
-
 	public DbSet<Country> Countries { get; set; }
 	public DbSet<Hotel> Hotels { get; set; }
 }
